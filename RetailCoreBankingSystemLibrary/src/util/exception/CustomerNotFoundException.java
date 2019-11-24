c8d47c0e155fd5debb7b8a6f95c691542b1c1328
Wong Jin Fu Shaun <sw_best@hotmail.com>
2019-11-25 04:53:12 +0800

    Update CustomerNotFoundException.java

--- RetailCoreBankingSystemLibrary/src/util/exception/CustomerNotFoundException.java
+++ RetailCoreBankingSystemLibrary/src/util/exception/CustomerNotFoundException.java
@@ -6,12 +6,8 @@ package util.exception;
  */
 public class CustomerNotFoundException extends Exception 
 {
-    public CustomerNotFoundException() 
-    {
-    }
+    public CustomerNotFoundException() {}
 
-    
-    
     public CustomerNotFoundException(String msg) 
     {
         super(msg);
